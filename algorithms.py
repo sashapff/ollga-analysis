f4edad6abfa631896ffa14402e88042c5367d671
Alexandra Ivanova <a.ivanova@niuitmo.ru>
2024-01-29 16:25:47 +0300

    *

--- algorithms.py
+++ algorithms.py
@@ -139,7 +139,7 @@ def not_find_optimum(x, n, f):
         return x.sum() != n
 
 
-def algorithm(n, lam, q, algo_fun, f, k, p, c, fitness_evaluations, filename, reevaluate=False):
+def algorithm(n, lam, q, algo_fun, f, k, p, c, fitness_evaluations, filename, reevaluate=True):
     n_iters = 0
     if is_quick(f):
         x = np.random.binomial(n, 1 / 2)
